d8970407f6b28984fd67fcf71c52564a65239c98
Yaroslav <yaroslav.lukashevich90@gmail.com>
2022-07-31 20:41:11 +0300

    added test ShiningPanda Page (HomeWork)

--- Panda_Plugins/Pages/panda_main_page.py
+++ Panda_Plugins/Pages/panda_main_page.py
@@ -4,8 +4,8 @@ from Panda_Plugins.Locators.shiningpanda_loc import PandaPageLoc
 link = "https://plugins.jenkins.io/shiningpanda/"
 
 
-class MainPage(BasePage):
-    def verify_panda_text(self):
-        panda_title_text = self.chrome.find_element(*PandaPageLoc.shinning_panda_title_loc).text
-        assert panda_title_text == 'ShiningPanda', 'Title is not ShiningPanda'
+class PandaMainPage(BasePage):
+    def verify_panda_text_title(self):
+        panda_text_title = self.chrome.find_element(*PandaPageLoc.shinning_panda_title_loc).text
+        assert panda_text_title == 'ShiningPanda', 'Title is not ShiningPanda'
 
